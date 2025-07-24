6025625bc4b49f8502ca8c03ba5827274d495138
Anele <anelenkomolity24@gmail.com>
2025-07-24 15:49:54 +0200

    added inline styling on new react app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return  (
+        <main>
+           <p style={{color:'red',fontSize:"12px",fontWeight: 'bold'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+
+    );
+}
+export default MainContent;
