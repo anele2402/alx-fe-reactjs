3d61f6a1e90ed9f8639907ea735599c03ac409db
Anele <anelenkomolity24@gmail.com>
2025-08-04 13:41:24 +0200

    installed react router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import SearchBar from './components/SearchBar'
