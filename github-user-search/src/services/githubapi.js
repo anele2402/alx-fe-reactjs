14ce6f5f62c785a654598ddd27a312bd7c3c7099
Anele <anelenkomolity24@gmail.com>
2025-08-08 10:44:18 +0200

    rectiified an API

--- github-user-search/src/services/githubapi.js
+++ github-user-search/src/services/githubapi.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const githubToken = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
 const api = axios.create({
-  baseURL: 'GET https://api.github.com/search/users?q=location:{location}+repos:>{minRepos}+{username}'
+  baseURL: 'GET https://api.github.com/search/users?'
+
 ,
   headers: {
     Authorization: githubToken ? `token ${githubToken}` : undefined,
