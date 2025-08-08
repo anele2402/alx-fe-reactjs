cb121e285dfa9d59e68a8acac8cabe2db67ea506
Anele <anelenkomolity24@gmail.com>
2025-08-08 10:47:30 +0200

    modified the api address

--- github-user-search/src/services/githubapi.js
+++ github-user-search/src/services/githubapi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const githubToken = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
 const api = axios.create({
-  baseURL: 'GET https://api.github.com/search/users?'
+  baseURL: "Get https://api.github.com/search/users?q"
 
 ,
   headers: {
