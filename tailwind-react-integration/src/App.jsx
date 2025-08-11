80f3b1924d1c8116329602eb073a582c006ea732
Anele <anelenkomolity24@gmail.com>
2025-08-11 13:00:56 +0200

    modified the user component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import UserProfile from './components/UserProfile'
+import UserProfile from './components/UserProfile';
 import './App.css'
 
 function App() {
@@ -14,4 +12,4 @@ function App() {
   )
 };
 
-export default App
+export default App;
