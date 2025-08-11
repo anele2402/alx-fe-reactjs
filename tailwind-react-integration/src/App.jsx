2f92195eb43bb61aa61e6c652867643bd5322c69
Anele <anelenkomolity24@gmail.com>
2025-08-11 13:33:26 +0200

    changed the component path

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import UserProfile from './components/UserProfile';
+import UserProfile from './components/UserProfile. jsx';
 import './App.css'
 
 function App() {
